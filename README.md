3159106bec28118cc7031597a7fd35ef700323db
Lukasz Grela <lukasz.grela@commelius.com>
2016-05-17 08:08:41 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# adapt-key-blocker
-An Adapt framework extension to block key pressing
+# Keyboard key press locker
+
+An extension to prevent the key presses as specified in the config.
+
+## Installation
+
+Download the ZIP and extract into the src > extensions directory and run an appropriate Grunt task.
+
+## Usage
+
+Add to the `config.json` extension config entry:
+
+```
+    "_keyBlocker": {
+        "_isEnabled": true,
+        "keys": [9, 13, 32]
+    }
+```
+
+* keys - an array of key codes to block
+
+## Notes
+
+When accessibility is required then this extension can make it work erraticly e.g. when TAB key blocked
+This may also prevent from working some of the input components.
+
+## Tested on
+Opera 37.0, Chrome 50, FF 45.0.2, IE 11
---
+++ js/adapt-key-blocker.js
@@ -0,0 +1,35 @@
+define(function (require) {
+
+    var Adapt = require("coreJS/adapt"),
+        locked = {},
+        handler = function (e) {
+            //IF NOT TAB KEY, RETURN
+            if (!locked[e.which]) return;
+            console.log("TAB KEY PREVENTED");
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        };
+
+    $('body').on("keydown", handler);
+
+
+        //initialize
+    Adapt.once('app:dataReady', function () {
+        var model = Adapt.config.get('_keyBlocker');
+        //console.log("adapt-key-blocker", "dataReady", model);
+        if (_.isUndefined(model) || (!_.isUndefined(model) && (model._isEnabled == false || !model.keys))) {
+            //dont use it - turned off
+            $('body').off("keydown", handler);
+            return;
+        } else {
+            //update keys to lock
+            if(model.keys) {
+                for(var i=0, max=model.keys.length; i<max; i++) {
+                    locked[model.keys[i]] = true;
+                }
+            }
+            //console.log("adapt-key-blocker", "locked", locked);
+        }
+    });
+});
